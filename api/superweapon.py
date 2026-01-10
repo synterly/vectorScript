c37cefbfebdbda6f4e142547f6511b72564b86d8
synterly <119030275+synterly@users.noreply.github.com>
2026-01-10 15:22:00 -0500

    Update superweapon.py

--- api/superweapon.py
+++ api/superweapon.py
@@ -31,6 +31,8 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "GET")
+        self.send_header("Access-Control-Max-Age", "86400")
         self.end_headers()
         self.wfile.write(response.encode())
 
