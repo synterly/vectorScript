250fed62ab6bbc443a012826e69164d87e226355
synterly <119030275+synterly@users.noreply.github.com>
2026-02-19 20:50:47 -0500

    Update girlboss.py

--- api/girlboss.py
+++ api/girlboss.py
@@ -4,7 +4,7 @@ import math
 import random
 
 def girlboss_power():
-    return random.random()
+    return random.random() * 1000
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -15,16 +15,16 @@ class handler(BaseHTTPRequestHandler):
         target_username = params.get('target', [''])[0]
 
         typer_power = girlboss_power()
-        if not target_username:
-            response = f"{typer_username} is totally girlbossing out right now, wowza! {typer_power} girlboss potential!"
+        if target_username == "":
+            response = f"{typer_username} is totally girlbossing out right now, wowza! {typer_power:.0f} girlboss potential!"
         else:
             target_power = girlboss_power()
             if target_power > typer_power:
-                response = f"uh oh! {typer_username}, you got outgirlbossed, with {typer_power} vs {target_power}"
+                response = f"uh oh! {typer_username}, you got outgirlbossed, with {typer_power:.0f} vs {target_power:.0f}"
             elif target_power < typer_power:
-                response = f"{typer_username} totally out girlbossed {target_username} with {typer_power} vs {target_power}"
+                response = f"{typer_username} totally out girlbossed {target_username} with {typer_power:.1f} vs {target_power:.1f}"
             elif target_power == typer_power:
-                response = f"{typer_username} and {target_username} tied, with {typer_power}, they should girlboss together and not against eachother"
+                response = f"{typer_username} and {target_username} tied, with {typer_power:.0f}, they should girlboss together and not against eachother"
         
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
